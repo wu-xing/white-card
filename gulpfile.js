e5b4ce068fc106787a6eb65d32c1a95d0b78c968
AbyChan <crystal.tyan@gmail.com>
2016-05-13 18:00:58 +0800

    sync

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
-    gulp.src('./scss/**/*.scss')
+    gulp.src('./scss/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
 	    browsers: ['>1%']
